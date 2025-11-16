aa7bc537e7467f857e2cc1844dd04b1dec4e5218
Adnan Shamim <adnanshamim1507@gmail.com>
2025-11-16 23:59:01 +0600

    implementing Forget/Reset Password 1.0

---
+++ Rental/Services/IEmailSender.cs
@@ -0,0 +1,7 @@
+﻿namespace Rental.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
---
+++ Rental/ViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rental.ViewModels
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+    }
+}
